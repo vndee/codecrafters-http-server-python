b20771eeff793c525dc90f1fbdef3a53bd09f840
Duy Huynh <vndee.huynh@gmail.com>
2024-11-26 18:11:37 +0700

    test

--- app/main.py
+++ app/main.py
@@ -3,7 +3,10 @@ import socket  # noqa: F401
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept() # wait for client
+    print("Connection from: ", addr)
+
+    conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
